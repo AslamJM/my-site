b33515e3bf08636e4881ef6edee7f2f0c9ec52f3
AslamJM <jma8744@gmail.com>
2023-05-24 00:03:09 +0530

    projects mdx added

--- src/components/home/RecentProjects.tsx
+++ src/components/home/RecentProjects.tsx
@@ -1,29 +1,42 @@
 import { FC } from "react";
 import ProjectCard from "../projects/ProjectCard";
+import { allProjects } from "contentlayer/generated";
 
 interface RecentProjectsProps {}
 
-const RecentProjects: FC<RecentProjectsProps> = ({}) => {
+const getProjects = async () => {
+  return allProjects[0];
+};
+
+// @ts-expect-error Server Component
+const RecentProjects: FC<RecentProjectsProps> = async ({}) => {
+  const project = await getProjects();
   return (
     <div className="flex items-center justify-center my-8">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <ProjectCard
           className="col-span-1"
-          title="my project"
-          image="test"
-          exerpt="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure illum sapiente eligendi necessitatibus quasi, nam odio accusantium cum rem quia."
+          title={project.title}
+          image={project.image}
+          exerpt={project.exerpt || ""}
+          source={project.github}
+          slug={project.slug}
         />
         <ProjectCard
           className="col-span-1"
-          title="my project"
-          image="test"
-          exerpt="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure illum sapiente eligendi necessitatibus quasi, nam odio accusantium cum rem quia."
+          title={project.title}
+          image={project.image}
+          exerpt={project.exerpt || ""}
+          source={project.github}
+          slug={project.slug}
         />
         <ProjectCard
           className="col-span-1"
-          title="my project"
-          image="test"
-          exerpt="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure illum sapiente eligendi necessitatibus quasi, nam odio accusantium cum rem quia."
+          title={project.title}
+          image={project.image}
+          exerpt={project.exerpt || ""}
+          source={project.github}
+          slug={project.slug}
         />
       </div>
     </div>
--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,11 +1,14 @@
 import { GithubIcon } from "@/icons/social";
+import Image from "next/image";
 import Link from "next/link";
 import { FC, HtmlHTMLAttributes } from "react";
 
 interface ProjectCardProps extends HtmlHTMLAttributes<HTMLDivElement> {
   title: string;
   image: string;
-  exerpt: string;
+  exerpt?: string;
+  source: string;
+  slug: string;
 }
 
 const ProjectCard: FC<ProjectCardProps> = ({
@@ -13,6 +16,8 @@ const ProjectCard: FC<ProjectCardProps> = ({
   exerpt,
   image,
   className,
+  source,
+  slug,
 }) => {
   return (
     <div
@@ -20,7 +25,14 @@ const ProjectCard: FC<ProjectCardProps> = ({
     >
       <div className="absolute w-[80px] h-[80px] bg-transparent rounded-tr-md border-dark border-t-4 border-r-4 -top-1 -right-1 -z-10" />
       <div className="absolute w-[80px] h-[80px] bg-transparent rounded-bl-md border-dark border-b-4 border-l-4 -left-1 -bottom-1 -z-10" />
-      <div className="h-[150px] bg-dark/50 rounded-md w-full"></div>
+      <div className="h-[150px] bg-dark/50 rounded-md w-full relative">
+        <Image
+          src={`/${image}`}
+          alt={title}
+          fill
+          className="absolute object-cover rounded-md border-[1px] border-dark"
+        />
+      </div>
       <h3 className="my-2 font-semibold text-xl">{title}</h3>
       <p className="text-md">{exerpt}</p>
       <div className="flex items-center justify-between mt-4">
@@ -29,7 +41,7 @@ const ProjectCard: FC<ProjectCardProps> = ({
             <GithubIcon />
           </div>
           <Link
-            href="/"
+            href={source}
             className="ml-1 text-dark hover:text-red-600 font-semibold underline underline-offset-2 "
           >
             Code
